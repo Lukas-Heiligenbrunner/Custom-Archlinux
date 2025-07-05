e9011b3ff184156e487287ca469dc8eb269e687e
Lukas Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2025-07-06 00:02:28 +0200

    add scripts

---
+++ airootfs/root/post-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.email "lukas.heiligenbrunner@gmail.com"
+git config --global user.name "Lukas Heiligenbrunner"
+
+sudo -u lukas gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
